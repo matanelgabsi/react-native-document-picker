f0c1fa398e52d8ca5f67e03ffbfb57bcc98f9526
matanelgabsi <matan.elgabsi@gmail.com>
2019-05-13 17:26:40 +0300

    Now support promises & additional picking sources

--- ios/RNDocumentPicker/RNDocumentPicker.m
+++ ios/RNDocumentPicker/RNDocumentPicker.m
@@ -28,21 +28,32 @@
 
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(showAsync:(NSDictionary *)options
+                  resolver:(RCTPromiseResolveBlock) resolve
+                  rejecter:(RCTPromiseRejectBlock) reject) {
+  [[self composeCallbacks] addObject:@{@"resolver": resolve, @"rejecter": reject}];
+  [self show:options];
+}
+
 RCT_EXPORT_METHOD(show:(NSDictionary *)options
                   callback:(RCTResponseSenderBlock)callback) {
+  [[self composeCallbacks] addObject:callback];
+  [self show:options];
+}
 
+- (void) show: (NSDictionary *) options {
     NSArray *allowedUTIs = [RCTConvert NSArray:options[@"filetype"]];
     UIDocumentMenuViewController *documentPicker = [[UIDocumentMenuViewController alloc] initWithDocumentTypes:(NSArray *)allowedUTIs inMode:UIDocumentPickerModeImport];
 
-    [[self composeCallbacks] addObject:callback];
+  
 
 
     documentPicker.delegate = self;
     documentPicker.modalPresentationStyle = UIModalPresentationFormSheet;
 
     UIViewController *rootViewController = [[[[UIApplication sharedApplication]delegate] window] rootViewController];
-    while (rootViewController.modalViewController) {
-        rootViewController = rootViewController.modalViewController;
+    while (rootViewController.presentedViewController) {
+        rootViewController = rootViewController.presentedViewController;
     }
 
     if ( IDIOM == IPAD ) {
@@ -51,8 +62,41 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
         [documentPicker.popoverPresentationController setSourceRect: CGRectMake([left floatValue], [top floatValue], 0, 0)];
         [documentPicker.popoverPresentationController setSourceView: rootViewController.view];
     }
+  
+  if (options[@"additionalItems"]) {
+    for (NSString *item in options[@"additionalItems"]) {
+      [documentPicker addOptionWithTitle:item image:nil order:UIDocumentMenuOrderLast handler:[self handlerForItem: item]];
+    }
+  }
 
     [rootViewController presentViewController:documentPicker animated:YES completion:nil];
+}
+
+- (void (^)(void)) handlerForItem: (NSString *) item {
+  __weak typeof(self) weakSelf = self;
+  return ^{
+    id callback = [[weakSelf composeCallbacks] lastObject];
+    [[weakSelf composeCallbacks] removeLastObject];
+    if (callback) {
+      if ([callback isKindOfClass:[NSDictionary class]]) {
+        RCTPromiseResolveBlock resolve = [callback objectForKey: @"resolver"];
+        resolve(item);
+      } else {
+        ((RCTResponseSenderBlock)callback)(@[[NSNull null], item]);
+      }
+    }
+  };
+}
+
+- (void)documentPickerWasCancelled:(UIDocumentPickerViewController *)controller {
+  RCTResponseSenderBlock callback = [[self composeCallbacks] lastObject];
+  [[self composeCallbacks] removeLastObject];
+  if ([callback isKindOfClass:[NSDictionary class]]) {
+    RCTPromiseResolveBlock resolve = [callback objectForKey: @"resolver"];
+    resolve(nil);
+  } else {
+    ((RCTResponseSenderBlock)callback)(@[[NSNull null], [NSNull null]]);
+  }
 }
 
 
@@ -75,7 +119,7 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
 
 - (void)documentPicker:(UIDocumentPickerViewController *)controller didPickDocumentAtURL:(NSURL *)url {
     if (controller.documentPickerMode == UIDocumentPickerModeImport) {
-        RCTResponseSenderBlock callback = [[self composeCallbacks] lastObject];
+        id callback = [[self composeCallbacks] lastObject];
         [[self composeCallbacks] removeLastObject];
 
         [url startAccessingSecurityScopedResource];
@@ -105,7 +149,12 @@ RCT_EXPORT_METHOD(show:(NSDictionary *)options
                 NSLog(@"%@", attributesError);
             }
 
-            callback(@[[NSNull null], result]);
+            if ([callback isKindOfClass:[NSDictionary class]]) {
+              RCTPromiseResolveBlock resolve = [callback objectForKey: @"resolver"];
+              resolve(result);
+            } else {
+              ((RCTResponseSenderBlock)callback)(@[[NSNull null], result]);
+            }
         }];
 
         [url stopAccessingSecurityScopedResource];
